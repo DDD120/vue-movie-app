0cdd04f08052f3dc6c11b5f9b8df913249bf6f64
DDD120 <dltnals9794@naver.com>
2021-07-27 16:56:11 +0900

    Create project

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/DDD120"
+      target="_blank">
+      (C){{ new Date().getFullYear() }} DDD120
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    padding: 70px;
+    text-align: center;
+    opacity: .3;
+    .logo {
+      display: block;
+      margin-bottom: 4px;
+    }
+  }
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'DDD120',
+    email: 'ddds0120m@gmail.com',
+    blog: 'https://github.com/DDD120',
+    phone: '+82-01-1234-5678',
+    image: 'https://avatars.githubusercontent.com/u/74492479?s=400&u=c0db28518b06bc727c4d28e93fbc6baf10d47c72&v=4'
+  })
+}
